cc5e4160663697800cd6e1a2bc2ca4180ff92a1c
Numed <ruschinets09@gmail.com>
2022-10-21 18:38:03 +0300

    Final project 0.2v

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const themeToggle = document.querySelector(".switch-toggle");
+const scrollUp = document.querySelector(".scroll-to");
 
 themeToggle.onchange = () => {
   if (themeToggle.classList.contains("active")) {
@@ -8,4 +9,17 @@ themeToggle.onchange = () => {
     document.body.classList.add("dark");
     themeToggle.classList.add("active");
   }
+};
+
+window.onscroll = () => {
+  window.pageYOffset > 1000
+    ? ((scrollUp.style.visibility = "visible"), (scrollUp.style.opacity = "1"))
+    : ((scrollUp.style.visibility = "hidden"), (scrollUp.style.opacity = "0"));
+};
+
+scrollUp.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 };
